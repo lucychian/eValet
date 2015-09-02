c81ce42aad61d0cf7c0dcc90b08eed10dff2d74a
interns <interns@mbywards-macbook-pro.local>
2015-09-02 14:58:51 -0700

    Frameworks

---
+++ eVALET/Car.swift
@@ -0,0 +1,9 @@
+//
+//  Car.swift
+//  eVALET
+//
+//  Created by interns on 9/2/15.
+//  Copyright © 2015 Drew Taylor. All rights reserved.
+//
+
+import Foundation
---
+++ eVALET/Controller/Users.swift
@@ -0,0 +1,9 @@
+//
+//  Users.swift
+//  eVALET
+//
+//  Created by interns on 9/1/15.
+//  Copyright © 2015 Drew Taylor. All rights reserved.
+//
+
+import Foundation
